710bd0211837ca4e89839f455c5de06b15515244
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-01 11:19:31 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -8,15 +8,15 @@ mkdir -p ./superset_home
 chown -R 1000:1000 ./superset_home
 
 cat << EOF > ./docker/pythonpath_dev/superset_config_docker.py
-SECRET_KEY = ${ADMIN_PASSWORD}
+SECRET_KEY = '${ADMIN_PASSWORD}'
 EMAIL_NOTIFICATIONS = False
-SMTP_HOST = ${SMTP_HOST}
+SMTP_HOST = '${SMTP_HOST}'
 SMTP_STARTTLS = False
 SMTP_SSL = False
 SMTP_USER = ''
 SMTP_PASSWORD = ''
-SMTP_PORT = ${SMTP_PORT}
-SMTP_MAIL_FROM = ${SMTP_MAIL_FROM}
+SMTP_PORT = '${SMTP_PORT}'
+SMTP_MAIL_FROM = '${SMTP_MAIL_FROM}'
 WTF_CSRF_ENABLED = False
 EOF
 
