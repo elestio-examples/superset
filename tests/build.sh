342da19cc77b68d8b5eb913ece22d49b84f85987
Prem chavhan <pchavhan@logiquad.com>
2023-12-13 21:15:49 +0530

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+sed -i "s~--password ~--password ${ADMIN_PASSWORD}~g" ./docker/docker-init.sh
+sed -i "s~--email ~--email ${ADMIN_EMAIL}~g" ./docker/docker-init.sh
 docker buildx build . --output type=docker,name=elestio4test/superset:latest | docker load
